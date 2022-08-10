8488ec5645a0dfc6cc7c39ff594737fa9ac612ad
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-08-10 07:29:23 +0700

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Konstantin Bolgar-Danchenko on 10.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
